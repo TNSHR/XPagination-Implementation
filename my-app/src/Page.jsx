3caa5d1af82d41fd51807dd506143b2174d05ee7
TNSHR <rohandw0@gmail.com>
2024-05-30 07:59:28 +0530

    Second comit for page logic

--- my-app/src/Page.jsx
+++ my-app/src/Page.jsx
@@ -3,11 +3,20 @@ import './page.css';
 
 const Page = ({ currentPage, totalPages, onPrevious, onNext }) => {
   return (
+    // <div className="pagination">
+    //   <button onClick={onPrevious} disabled={currentPage === 1}>Previous</button>
+    //   <span>{` ${currentPage} `}</span>
+    //   <button onClick={onNext} disabled={currentPage === totalPages}>Next</button>
+    // </div>
     <div className="pagination">
-      <button onClick={onPrevious} disabled={currentPage === 1}>Previous</button>
-      <span>{` ${currentPage} `}</span>
-      <button onClick={onNext} disabled={currentPage === totalPages}>Next</button>
-    </div>
+    {currentPage > 1 && (
+      <button onClick={onPrevious}>Previous</button>
+    )}
+    <span>{` ${currentPage} `}</span>
+    {currentPage < totalPages && (
+      <button onClick={onNext}>Next</button>
+    )}
+  </div>
   );
 };
 
