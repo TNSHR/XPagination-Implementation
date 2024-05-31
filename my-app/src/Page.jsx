c4a2ce38f0f735f031ef327f69933075f034c710
TNSHR <rohandw0@gmail.com>
2024-05-31 07:37:04 +0530

    final2

--- my-app/src/Page.jsx
+++ my-app/src/Page.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 const Page = ({ currentPage, totalPages, onPrevious, onNext }) => {
   return (
     <div className="pagination">
-      <button onClick={onPrevious} disabled={currentPage === 1}>
+      <button
+        onClick={currentPage > 1 ? onPrevious : null}
+        disabled={currentPage === 1}
+      >
         Previous
       </button>
       <span>{` ${currentPage} `}</span>
-      <button onClick={onNext} disabled={currentPage === totalPages}>
+      <button
+        onClick={currentPage < totalPages ? onNext : null}
+        disabled={currentPage === totalPages}
+      >
         Next
       </button>
     </div>
