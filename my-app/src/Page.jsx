93f0a4398f3fdd9ec03d8e8f9af2a4a82d170f2c
TNSHR <rohandw0@gmail.com>
2024-05-31 07:29:20 +0530

    final

--- my-app/src/Page.jsx
+++ my-app/src/Page.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import './page.css';
 
 const Page = ({ currentPage, totalPages, onPrevious, onNext }) => {
   return (
-    // <div className="pagination">
-    //   <button onClick={onPrevious} disabled={currentPage === 1}>Previous</button>
-    //   <span>{` ${currentPage} `}</span>
-    //   <button onClick={onNext} disabled={currentPage === totalPages}>Next</button>
-    // </div>
     <div className="pagination">
-    {currentPage > 0 && (
-      <button onClick={onPrevious}>Previous</button>
-    )}
-    <span>{` ${currentPage} `}</span>
-    {currentPage === totalPages && (
-      <button onClick={onNext}>Next</button>
-    )}
-  </div>
+      <button onClick={onPrevious} disabled={currentPage === 1}>
+        Previous
+      </button>
+      <span>{` ${currentPage} `}</span>
+      <button onClick={onNext} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
   );
 };
 
